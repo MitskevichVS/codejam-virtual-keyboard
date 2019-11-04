f58fa12ed37bce386591467e622194b35d1d8f7d
Mitskevich Victor <mv2.mx@yandex.ru>
2019-11-04 11:57:14 +0300

    fix: capslock hold event

--- src/javascript/keyboard/checkRegisterButtons.js
+++ src/javascript/keyboard/checkRegisterButtons.js
@@ -1,11 +1,11 @@
 const checkRegisterButtons = (setRegister, event) => {
   if (event.shiftKey && event.type === 'keydown' && event.key === 'Shift') {
     if (event.repeat) return;
-
     setRegister();
   } else if (event.key === 'Shift' && event.type === 'keyup') {
     setRegister();
   } else if (event.key === 'CapsLock' && event.type === 'keydown') {
+    if (event.repeat) return;
     setRegister();
     const capsLockButton = document.querySelector('.CapsLock');
     capsLockButton.classList.toggle('wrapper__container__button-pressed');
